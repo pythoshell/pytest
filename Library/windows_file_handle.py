83be79b14a57cd6b7927685ee4df5dcea23062af
pramodtvm <pramodur@gmail.com>
2024-06-15 19:09:19 +0530

    Commiting Code

---
+++ Library/windows_file_handle.py
@@ -0,0 +1,28 @@
+import os
+import re
+
+def filter_directories(base_path, pattern):
+    # Compile the regex pattern for efficiency
+    regex = re.compile(pattern)
+
+    # Iterate over directories
+    for root, dirs, files in os.walk(base_path):
+        for directory in dirs:
+            # Check if the directory name matches the regex pattern
+            if regex.match(directory):
+                yield os.path.join(root, directory)
+
+
+# Usage example
+if __name__ == "__main__":
+    base_path = r"C:\PowerShell_Tests\engine"  # Change this to your base folder path
+    pattern = r""  # Change this to your regex pattern
+
+    matching_directories = filter_directories(base_path, pattern)
+    count = 0
+    for dir_path in matching_directories:
+
+        print(dir_path)
+        print(matching_directories)
+        count = count + 1
+        print(count)
--- data/excel_data.py
+++ data/excel_data.py
@@ -1,10 +1,12 @@
 import pandas as pd
-
 import os
 
 # Get the current working directory
 current_directory = os.getcwd()
 print(current_directory)
 
-excel_df = pd.read_csv('datacsv.csv')
-print(excel_df.head())
+#encoding = result['encoding']
+df = pd.read_csv("C:\pyauto\pytest\data\datacsv.csv",encoding='latin1')
+print(df.tail())
+c = df.columns
+print(c)
